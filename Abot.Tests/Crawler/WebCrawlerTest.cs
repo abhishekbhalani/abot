61f4865687ae1c20bf5b6fb82ba486e0a5838241
sjdirect <sjdirect@gmail.com>
2012-08-19 10:16:05 +0000

    broken

--- Abot.Tests/Crawler/WebCrawlerTest.cs
+++ Abot.Tests/Crawler/WebCrawlerTest.cs
@@ -31,13 +31,34 @@ namespace Abot.Tests.Crawler
         }
 
         [Test]
-        public void Crawl()
+        public void Crawl_SchedulesHomePage()
+        {   
+            _unitUnderTest.Crawl(_rootUri);
+
+            _fakeScheduler.Verify(f => f.Add(It.Is<PageToCrawl>(p => p.Uri == _rootUri)));
+        }
+
+        [Test]
+        public void Crawl_CallsDependencies()
         {   
             //TODO setup mocks so that it returns links on the first request but not on the rest
+            _fakeHttpRequester.Setup(f => f.MakeRequest(_rootUri)).Returns(new CrawledPage(_rootUri));
+            _fakeHyperLinkParser.Setup(f => f.GetHyperLinks(It.IsAny<Uri>(), It.IsAny<string>()));
+
+            FifoScheduler _dummyScheduler = new FifoScheduler();
+
+            _unitUnderTest = new WebCrawler(_fakeThreadManager.Object, _dummyScheduler, _fakeHttpRequester.Object, _fakeHyperLinkParser.Object);
 
             _unitUnderTest.Crawl(_rootUri);
 
             _fakeScheduler.Verify(f => f.Add(It.Is<PageToCrawl>(p => p.Uri == _rootUri)));
+            _fakeHttpRequester.Verify(f => f.MakeRequest(f.MakeRequest(_rootUri)));
+            _fakeHyperLinkParser.Verify(f => f.GetHyperLinks(_rootUri, It.IsAny<string>()));
+        }
+
+        [Test]
+        public void Crawl_EventsFire()
+        {
         }
     }
 }
