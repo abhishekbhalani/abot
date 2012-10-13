b8d89a2dc838f4c73dce69a7ca1c7d2022fb37a1
sjdirect <sjdirect@gmail.com>
2012-10-13 22:16:03 +0000

    Renamed Abot.Console to Abot.Demo

---
+++ Abot.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Abot.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate unity container
+
+            //Create instance of crawler
+
+            //Run
+        }
+    }
+}
