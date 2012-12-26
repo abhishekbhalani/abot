6bb8c8a351ca1eed095cba53f845839178d521a3
sjdirect <sjdirect@gmail.com>
2012-12-26 00:23:01 +0000

    55: Added stubs for IProcessMemoryLimiter

---
+++ Abot/Core/ProcessMemoryLimiter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Abot.Core
+{
+    public interface IProcessMemoryLimiter
+    {
+        int GetMemoryBeingUsedInMb();
+        bool IsOverLimit();
+    }
+
+    public class ProcessMemoryLimiter : IProcessMemoryLimiter
+    {
+        int _limitInMb = 0;
+
+        public ProcessMemoryLimiter(int limitInMb)
+        {
+            if (limitInMb < 0)
+                throw new ArgumentException("limitInMb must be positive");
+
+            _limitInMb = limitInMb;
+        }
+
+        public bool IsOverLimit()
+        {
+            if(_limitInMb < 1)
+                return false;
+
+            return _limitInMb > GetMemoryBeingUsedInMb();
+        }
+
+        public virtual int GetMemoryBeingUsedInMb()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
